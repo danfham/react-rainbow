5b4fdf916651fc9eb781565d5cbedb4f6d2a9230
danfham <dan.cunningham91@icloud.com>
2023-08-12 09:34:21 -0400

    finalized react rainbow colors can be added to display dynamically

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 // Make sure to import the component we just built:
 import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
 
 function App(){
-    let colors = [
+    let [colors, setColors] = useState([
         'violet', 'blue',
         'lightblue', 'green',
         'greenyellow', 'yellow',
         'orange', 'red'
-    ]
+    ])
     
     let colorMap = colors.map((color, i) => {
         return (
@@ -16,9 +17,14 @@ function App(){
         )
     })
 
+    const addColor = (newColor)=>{
+        setColors([...colors,newColor])
+    }
+
     return (
         <div className="App">
             {colorMap}
+            <ColorForm addColor={addColor} />
         </div>
     )
 }
